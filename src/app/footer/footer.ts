3ac1f71aea5532dec9b7eab70c7aede1762ba61b
1590LACM <20alejandro26@gmail.com>
2026-08-27 20:27:19 -0500

    Descripción corta de lo que cambiaste

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  constructor(private router: Router) {}
+
+  irAlInicioDePagina(event: Event, ruta: string): void {
+    event.preventDefault();
+
+    if (this.router.url === ruta || (ruta === '/' && this.router.url === '')) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      return;
+    }
+
+    this.router.navigateByUrl(ruta).then(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  }
+}
